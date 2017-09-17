e2dcd32ac6c4582470e36ca613284dbf7b1d12a6
Lise <nakache.lise@gmail.com>
2017-09-17 16:42:01 +0300

    final project

--- public/main.js
+++ public/main.js
@@ -1,7 +1,64 @@
+//Local storage
+var STORAGE_ID = 'weather-app';
+var saveToLocalStorage = function () {
+    localStorage.setItem(STORAGE_ID, JSON.stringify(citiesInfos));
+    console.log(getFromLocalStorage())
+}
+var getFromLocalStorage = function () {
+    return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+}
+
+//Templates
 var source = $('#weather-template').html();
 var template = Handlebars.compile(source)
 
 var citiesInfos = [];
+
+var prepInfos = function (data) {
+    var name = data.name;
+    var tempC = (data.main.temp) - 273.15;
+    var C = tempC.toFixed(0);
+    var tempF = (data.main.temp) * 9 / 5 - 459.67;
+    var F = tempF.toFixed(0);
+
+    var fullDate = new Date();
+    var month = fullDate.getMonth() + 1;
+    var twoDigitMonth = function () {
+        if (month < 10) {
+            return '0' + month;
+        }
+    }
+    var currentMonth = twoDigitMonth();
+    var date = fullDate.getDate() + "/" + currentMonth + "/" + fullDate.getFullYear();
+
+    var dt = new Date();
+    var hours = dt.getHours();
+    var minutes = dt.getMinutes();
+    var currentMinute = function () {
+        console.log(minutes)
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+            return hours + ':' + minutes;
+        } else {
+            return hours + ':' + minutes;
+        }
+    }
+    var time = currentMinute();
+    var typeOfWeather = data.weather[0].main;
+
+
+    //Obj to keep all the infos
+    var infoObj = {
+        name: name,
+        C: C,
+        F: F,
+        date: date,
+        time: time,
+        typeOfWeather: typeOfWeather,
+        comments: []
+    }
+    addInfo(infoObj);
+}
 
 var fetch = function () {
     var cityValue = $('.get-city').val();
@@ -11,23 +68,9 @@ var fetch = function () {
         url: "http://api.openweathermap.org/data/2.5/weather?q=" + cityValue + "&APPID=d703871f861842b79c60988ccf3b17ec",
 
         success: function (data) {
+            $('.get-city').val('')
             console.log(data)
-
-            var name = data.name;
-            var tempC = (data.main.temp) - 273.15;
-            var C = tempC.toFixed(0);
-            var tempF = (data.main.temp) * 9 / 5 - 459.67;
-            var F = tempF.toFixed(0);
-
-            var fullDate = new Date();
-            var twoDigitMonth = ((fullDate.getMonth().length + 1) === 1) ? (fullDate.getMonth() + 1) : '0' + (fullDate.getMonth() + 1);
-            var date = fullDate.getDate() + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
-
-            var dt = new Date();
-            var time = dt.getHours() + ":" + dt.getMinutes();
-
-            addInfo(name, C, F, date, time)
-
+            prepInfos(data)
         },
         error: function (jqXHR, textStatus, errorThrown) {
             console.log(textStatus);
@@ -35,42 +78,75 @@ var fetch = function () {
     });
 };
 
-var addInfo = function (cityName, celsius, farenheit, currentDate, currentTime, comments) {
-    var cityObj = {
-        name: cityName,
-        C: celsius,
-        F: farenheit,
-        date: currentDate,
-        time: currentTime,
-        comments: []
-    }
-    $('.get-city').val('')
-    // add to the array
-    citiesInfos.push(cityObj);
-    // render to page
-    // $('.results').empty();
-    var newHTML = template(cityObj);
-    $('.results').append(newHTML);
+var conditionWeather = function (typeOfWeather) {
+    var lastResult = $('.result').last();
+    lastResult.css('background-image', 'url("./' + typeOfWeather + '.jpg")');
 }
 
+
+
+var addInfo = function (infoObj) {
+    citiesInfos.push(infoObj);
+    // saveToLocalStorage()
+    renderPage();
+}
+
+var renderInfo = function (infoObj, typeOfWeather) {
+    var newHTML = template(infoObj);
+    $('.results').append(newHTML);
+    conditionWeather(typeOfWeather);
+}
+
+var removeInfo = function (btn) {
+    //remove from array
+    var city = $(btn).closest('.infos');
+    var cityIndex = city.index();
+    citiesInfos.splice(cityIndex, 1);
+    // saveToLocalStorage();
+    //remove from page, understands that one item less
+    renderPage();
+}
 
 var addComments = function (btn) {
-//add comment to the array
-    var commentVal = $(btn).siblings('input').val();
+    //add comment to the array
+    var commentVal = $(btn).closest('.input-group').find('input').val();
     var city = $(btn).closest('.result')
-    var postComment = city.find('.comment-HTML')
+    var postComment = city.find('.comment-HTML').find('.list-comments');
     var cityIndex = city.index();
     citiesInfos[cityIndex].comments.push(commentVal);
-//render comments
-    
-    $(postComment).append(commentVal);
+    // saveToLocalStorage();
+    //render comments
+    $(postComment).append('<li>' + commentVal + '</li>');
+}
+
+//render the page item by item
+var renderPage = function () {
+    $('.results').empty();
+    for (var i = 0; i < citiesInfos.length; i++) {
+        console.log(citiesInfos[i]);
+        renderInfo(citiesInfos[i], citiesInfos[i].typeOfWeather);
+    }
+
 }
 
 
+//Click handlers
+$('#weather_form').on("submit", function (event) {
+    event.preventDefault()
+    fetch();
+})
 
+$('.results').on("click", ".fa-trash", function () {
+    removeInfo(this);
+})
 
-
-$('#weather_form').submit(fetch);
-$('.results').on("click",".comment-btn",function() {
+$('.results').on("click", ".comment-btn", function (event) {
+    event.preventDefault()
     addComments(this);
 });
+
+
+//update the var from local storage 
+// citiesInfos = getFromLocalStorage();
+// first render of the page 
+renderPage();
